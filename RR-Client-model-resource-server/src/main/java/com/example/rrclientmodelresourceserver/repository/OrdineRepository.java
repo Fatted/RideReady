5aa1d799bea0250eec99ec9462e9adf247d2323b
Antonio D'Amato <A.DAMATO73@STUDENTI.UNISA.IT>
2024-06-26 12:25:30 +0200

    Completo Api Acquisto con logger e commenti

--- RR-Client-model-resource-server/src/main/java/com/example/rrclientmodelresourceserver/repository/OrdineRepository.java
+++ RR-Client-model-resource-server/src/main/java/com/example/rrclientmodelresourceserver/repository/OrdineRepository.java
@@ -1,7 +1,19 @@
 package com.example.rrclientmodelresourceserver.repository;
 
 import com.example.rrclientmodelresourceserver.model.OrdineEntity;
+import com.example.rrclientmodelresourceserver.model.UtenteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrdineRepository extends JpaRepository<OrdineEntity, Long> {
+
+
+
+
+
+    //cerco tutti gli ordini di acquisto di un utente in base al suo id
+    @Query("SELECT o FROM OrdineEntity o WHERE o.idUtente = ?1 AND o.tipo = 'acquisto' ")
+    List<OrdineEntity> findByIdUtenteAndAcquisto(UtenteEntity idUtente);
 }
